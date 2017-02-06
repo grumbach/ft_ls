243c44f94c80b44853af3e1fea03c815e55445fc
Anselme GRUMBACH <agrumbac@e2r1p1.42.fr>
2017-02-07 00:58:24 +0100

    backend lst for files DONE

--- srcs/ft_ls_back_args.c
+++ srcs/ft_ls_back_args.c
@@ -6,7 +6,7 @@
 /*   By: agrumbac <agrumbac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/04 05:50:09 by agrumbac          #+#    #+#             */
-/*   Updated: 2017/02/05 19:00:04 by agrumbac         ###   ########.fr       */
+/*   Updated: 2017/02/07 00:54:32 by agrumbac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int				ft_ls_error_file_dir(const char *path)
 	DIR					*dirp;
 	struct dirent		*file;
 
+	if (!path)
+		return (1);
 	if ((dirp = opendir(path)))
 	{
 		(void)closedir(dirp);
@@ -58,6 +60,35 @@ static void		free_char(void *str, size_t size)
 	ft_memdel(&str);
 }
 
+static void		ls_caller(const t_list *lst, const char *flags, int args)
+{
+	t_list		*tmp;
+	t_list		*files;
+
+	while (lst)
+	{
+		files = NULL;
+		if (ft_ls_error_file_dir((char*)(lst->content)) == 2)
+			while (lst)
+			{
+				if (ft_ls_error_file_dir((char*)(lst->content)) == 2)
+				{
+					tmp = ft_ls_back((char*)(lst->content), flags);
+					if (!files)
+						files = tmp;
+					else
+						ft_lstaddend(&files, tmp);
+					lst = lst->next;
+				}
+				else
+					break ;
+			}
+		(files ? ft_ls_front(files, flags, 0, args) :
+		ft_ls((char*)(lst->content), flags, args));
+		lst = (files ? lst : lst->next);
+	}
+}
+
 void			ft_ls_args(int ac, char **av, int i, const char *flags)
 {
 	t_list		*lst;
@@ -78,11 +109,6 @@ void			ft_ls_args(int ac, char **av, int i, const char *flags)
 	}
 	lst = ft_lstsort(lst, &sort_args);
 	args = ft_lstsize(lst);
-	tmp = lst;
-	while (tmp)
-	{
-		ft_ls((char*)(tmp->content), flags, args);
-		tmp = tmp->next;
-	}
+	ls_caller(lst, flags, args);
 	ft_lstdel(&lst, &free_char);
 }
