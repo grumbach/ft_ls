25bf0746a1b44da0f58ad061b90981e6d291d0ae
Anselme Grumbach <agrumbac@student.42.fr>
2017-02-14 11:24:18 +0100

    swiched color order

--- srcs/ft_ls_front_colors.c
+++ srcs/ft_ls_front_colors.c
@@ -6,7 +6,7 @@
 /*   By: Anselme <Anselme@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/14 10:14:40 by Anselme           #+#    #+#             */
-/*   Updated: 2017/02/14 10:39:59 by Anselme          ###   ########.fr       */
+/*   Updated: 2017/02/14 11:22:47 by Anselme          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,17 @@ int				ft_ls_colors(const t_pls *info, const char *flags)
 		return (0);
 	if (info->mode[0] == 'd')
 		return (4);
-	if (info->mode[3] == 'x')
-		return (1);
 	if (info->mode[0] == 'l')
 		return (3);
-	if (info->mode[1] == '-')
-		return (7);
-	if (info->major >= 0)
-		return (6);
 	if (info->mode[0] == 'b')
 		return (5);
 	if (info->mode[0] == 'c')
 		return (2);
+	if (info->major >= 0)
+		return (6);
+	if (info->mode[3] == 'x')
+		return (1);
+	if (info->mode[1] == '-')
+		return (7);
 	return (0);
 }
